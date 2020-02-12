716eb9b7dd6eb0248889f53c0f0bcd4c9b8f07c0
Enrique Lacal Bereslawski <Enrique.Lacal.Bereslawski@ibm.com>
2020-02-12 11:49:07 +0000

    Fix permissions issue

--- ci/git_setup.sh
+++ ci/git_setup.sh
@@ -1,9 +1,8 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "$TRAVIS_BRANCH" ]
 then
-  export REPO_PATH= "$TRAVIS_BUILD_DIR/$TRAVIS_REPO_SLUG"
-  rm -rf $REPO_PATH
-  git clone https://github.com/$TRAVIS_REPO_SLUG.git $REPO_PATH
-  cd $REPO_PATH
+  rm -rf $TRAVIS_BUILD_DIR
+  git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_BUILD_DIR
+  cd $TRAVIS_BUILD_DIR
   git status -sb
   git reset --hard $TRAVIS_COMMIT
 fi
